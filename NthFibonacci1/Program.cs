eedfb71cafaa314c9d412804aef0283ddbbbdee9
Giorgi Mshvildadze <giorgimshvildadze@credo.ge>
2023-10-17 17:41:01 +0400

    first commit

---
+++ NthFibonacci1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NthFibonacci;
+
+class Program
+{
+
+
+    public static long CalculateFibonacci(int n)
+    {
+        int a = 0;
+        int b = 1;
+
+        int c = 0;
+        List<int> Fibonacci = new List<int>();
+
+
+        Fibonacci.Add(a);
+        Fibonacci.Add(b);
+        for (int i = 0; i <= n; i++)
+        {
+
+            c = a + b;
+
+            a = b;
+            b = c;
+            Fibonacci.Add(c);
+
+
+        }
+
+        //  Console.WriteLine(Fibonacci[n-1]);
+
+        return Fibonacci[n - 1];
+
+    }
+    static void Main(string[] args)
+    {
+
+
+
+        Console.WriteLine(CalculateFibonacci(Convert.ToInt32(Console.ReadLine()));
+
+
+    }
+}
---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TempConverter;
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        int Temperature;
+
+
+
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Enter Temperature in C:");
+                Temperature = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Temperature * 9 / 5 + 32);
+                Console.WriteLine("Enter Temperature in F: ");
+                Temperature = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine((Temperature - 32) * 5 / 9);
+
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Please enter valid number:");
+            }
+        }
+
+
+
+
+
+
+    }
+}
