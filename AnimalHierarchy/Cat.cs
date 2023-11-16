879bf476a425eededfe8590d7145d32b9e2ad482
GiorgiMshvildadze <giorgimshvildadze66@gmail.com>
2023-11-17 02:45:03 +0400

    17/11

---
+++ AnimalHierarchy/Cat.cs
@@ -0,0 +1,28 @@
+﻿using AnimalsHierarchy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHierarchy
+{
+    public class Cat : Animal
+    {
+        public string Breed { get; set; }
+
+        public Cat(string name, int age, string breed)
+        {
+
+            Name = name;
+            Age = age;
+            Breed = breed;
+            Sound = "Meow!!";
+            Console.WriteLine($"A Cat Named {Name} is Created. It is {Age} years old and it is a {Breed}");
+        }
+        public override void MakeSound()
+        {
+            base.MakeSound();
+        }
+    }
+}
