05106890743b93508d786292f8c1df4386667652
GiorgiMshvildadze <giorgimshvildadze66@gmail.com>
2023-11-29 19:40:00 +0400

    29/11

---
+++ Assignments/BurgerBuilder/PresetBurger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerBuilder
+{
+    public class PresetBurger
+    {
+        public static void ChoosePresetBurger(string? choosePreset)
+        {
+            switch (choosePreset)
+            {
+                case "1":
+                    Console.WriteLine("Cheeseburger Contains:");
+                    Burger Cheeseburger = new Burger();
+                    string[] cheeseburger = Cheeseburger.BuildCheeseBurger();
+                    foreach (var ingredient in cheeseburger)
+                    {
+                        Console.WriteLine(ingredient);
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("Hamburger Contains:");
+                    Burger hamBurger = new Burger();
+                    string[] hamburger = hamBurger.BuildHamburger();
+                    foreach (var ingredient in hamburger)
+                    {
+                        Console.WriteLine(ingredient);
+                    }
+                    break;
+                case "3":
+                    Console.WriteLine("Special Burger Contains:");
+                    Burger SpecialBurger = new Burger();
+                    string[] specialBurger = SpecialBurger.BuildSpecialBurger();
+                    foreach (var ingredient in specialBurger)
+                    {
+                        Console.WriteLine(ingredient);
+                    }
+                    break;
+
+
+            }
+
+
+        }
+    }
+}
---
+++ Assignments/TicTacToe/Variables.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Variables
+    {
+        public static string player1turn = "Player 1 turn to move, Enter Coordinates:";
+        public static string player2turn = "Player 2 turn to move, Enter Coordinates";
+        public static string player1sign = "X ";
+        public static string player2sign = "O ";
+        public static string boardEmptyPlace = "- ";
+        public static string enterCoordinateX = "Enter Coordinate X (0,1,2)";
+        public static string enterCoordinateY = "Enter Coordinate Y (0,1,2)";
+        public static string[,] Board = new string[3, 3];
+        public static bool SwitchPlayerTurns = true;
+        public static int coordinateX;
+        public static int coordinateY;
+
+
+        
+    }
+}
